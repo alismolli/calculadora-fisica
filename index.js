f4c06ac236dccfe9386bfe76f98410be528ae974
alismolli <amollinedoc@est.emi.edu.bo>
2021-09-22 19:54:15 -0400

    avance de clase

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World") 
+function areaDelCuadrado(ladoDelCuadrado) {
+    const area = ladoDelCuadrado * ladoDelCuadrado;
+    return area
+}
+
+const perimetroDelCuadrado = ladoDelCuadrado => ladoDelCuadrado * 4;
+
+// const ladoDelCuadrado = 6;
+// const PI = 3.1416;
+// let variableQueCambia
+// const area = lado*lado;
+// const perimetro = lado * 4;
+
+// console.log(`Àrea ${area}`);
+// console.group({area:area, perimetro:perimetro})

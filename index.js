b63047ae892639eb4837bd890d41837dd089804d
alismolli <amollinedoc@est.emi.edu.bo>
2021-10-14 05:57:04 -0400

    detalles

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-function areaDelCuadrado(ladoDelCuadrado) {
-    const area = ladoDelCuadrado * ladoDelCuadrado;
-    return area
+const perimetroDelCuadrado = (ladoDelCuadrado) => ladoDelCuadrado * 4;
+const areaDelCuadrado = (ladoDelCuadrado) => ladoDelCuadrado * ladoDelCuadrado;
+
+var calcularAreaCuadrado = () => {
+
+    const ladoDelCuadrado = document.querySelector("#lado-cuadrado").value;
+    const resultado = areaDelCuadrado(ladoDelCuadrado)
+    document.querySelector("#resultado").innerHTML = resultado
 }
+const perimetroDelTriangulo = (ladoDelTriangulo) => (ladoDelTriangulo*3);
+const areaDelTriangulo = (baseDelTriangulo,alturaDelTriangulo) => (baseDelTriangulo * alturaDelTriangulo)/2;
 
-const perimetroDelCuadrado = ladoDelCuadrado => ladoDelCuadrado * 4;
+var calcularAreaTriangulo = () => {
 
+    const baseDelTriangulo = document.querySelector("#base-triangulo").value;
+    const alturaDelTriangulo = document.querySelector("#altura-triangulo").value;
+    const resultado = areaDelTriangulo(baseDelTriangulo,alturaDelTriangulo)
+    document.querySelector("#resultado").innerText = resultado
+}
+var calcularPerimetroTriangulo = () => {
+
+    const ladoDelTriangulo = document.querySelector("#lado-triangulo").value;
+    const resultado = perimetroDelTriangulo(ladoDelTriangulo)
+    document.querySelector("#resultado").innerHTML = resultado
+}
 // const ladoDelCuadrado = 6;
 // const PI = 3.1416;
 // let variableQueCambia
